41936953aa0efd3973c93742eb577ee499bb5d8b
Rémi <remi.adon@gmail.com>
2019-08-20 22:08:31 +0200

    [RM] deprecated benchs/

--- benchs/profile_clostream.py
+++
@@ -1,8 +0,0 @@
-from src.clostream import CloStream
-import pandas as pd
-
-columns = ['song_id','user_id', 'artist_id', 'provider_id', 'ip']
-df = pd.read_csv('listen-20131115.log', names=columns, nrows=1000)
-
-cs = CloStream()
-cs.add_df(df)
