e7e6642ea3102e695abdaa1dd3a408f07d02f37f
Rémi <remi.adon@gmail.com>
2019-08-20 22:07:02 +0200

    simplify TIDLister

--- transformers.py
+++ transformers.py
@@ -17,31 +17,19 @@ class TIDLister(defaultdict):
     def __init__(self, n_jobs=1, verbose=0):
         self.n_jobs = n_jobs
         self.verbose = verbose
+        self.low = 0
+        self.high = 0
         super(TIDLister, self).__init__(RoaringBitmap)
 
-    def add(self, obj):
-        return self.add_df_par(obj) if isinstance(obj, pd.DataFrame) else self.add_series(obj)
+    def add(self, transaction):
+        for item in transaction:
+            self[item].add(self.high)
+        self.high += 1
 
-    def add_series(self, s):
-        for row in s.iteritems():
-            index, items = row[0], row[1:]
-            for i in items[0]:
-                self[i].add(index)
-        return self
-
-    def add_df_col(self, col):
-        for idx, value in col.iteritems():
-            item = '{}_{}'.format(col.name, value).encode('utf-8')
-            self[item].add(idx)
-
-        return self
-
-    def add_df_par(self, df):
-        df = df.reset_index()
-        n_jobs = self.n_jobs if df.shape[0] > 200 else 1
-        p = joblib.Parallel(n_jobs=n_jobs, verbose=self.verbose, backend='threading')
-        p(joblib.delayed(self.add_df_col)(df[col]) for col in df.columns)
-        return self
+    def remove(self, transaction):
+        for item in transaction:
+            self[item].remove(self.low)
+        self.low += 1
 
     def trim(self, min_supp):
         keys = {k for k,v in self.items() if len(v) < min_supp}
