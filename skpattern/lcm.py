044cea01f0afeb2e4ffd5c9712b96aa6fe73727a
Rémi <remi.adon@gmail.com>
2019-09-01 16:55:49 +0200

    reimplement LCM with a item_to_tids as SortedDict

--- skpattern/lcm.py
+++ skpattern/lcm.py
@@ -1,8 +1,7 @@
-import itertools
 import pandas as pd
 from collections import defaultdict
 from roaringbitmap import RoaringBitmap
-from collections import defaultdict
+from sortedcontainers import SortedDict
 
 
 class LCM(object):
@@ -11,9 +10,8 @@ class LCM(object):
                  n_jobs=1,
                  verbose=1,
                  return_tids=False):
-        # TODO = add filter_fn
         self.min_support = min_supp
-        self.item_to_tids = defaultdict(RoaringBitmap)
+        self.item_to_tids = SortedDict()
         self.verbose = verbose
         self.format = self._format_with_tids if return_tids else self._format
         self.extra_col = 'tids' if return_tids else 'support'
@@ -22,16 +20,17 @@ class LCM(object):
     def add(self, transaction):
         transaction = frozenset(transaction)
         for item in transaction:
-            self.item_to_tids[item].add(self.n_transactions)
+            if item in self.item_to_tids:
+                self.item_to_tids[item].add(self.n_transactions)
+            else:
+                self.item_to_tids[item] = RoaringBitmap([self.n_transactions])
         self.n_transactions += 1
 
     def discover_yield(self):
-        sorted_keys = sorted(self.item_to_tids.keys())
-        scope_keys = self.item_to_tids.keys()
-        for idx, key in enumerate(sorted_keys):
-            key_idxs = self.item_to_tids[key]
+        items = [e for e in self.item_to_tids.items() if len(e[1]) >= self.min_support]
+        for key, key_idxs in items:
             if len(key_idxs) >= self.min_support:
-                yield from self._inner(frozenset(), key_idxs, key, scope_keys)
+                yield from self._inner(frozenset(), key_idxs, key, items)
 
     def discover(self):
         data = self.discover_yield()
@@ -43,30 +42,25 @@ class LCM(object):
     def _format(self, p_prime, p_idxs):
         return p_prime, len(p_idxs)
 
-    def get_new_scope_keys(self, new_keys, p_idxs):
-        new_scope_keys = set()
-        for key in new_keys:
-            inter_len = p_idxs.intersection_len(self.item_to_tids[key])
-            if inter_len >= self.min_support:
-                new_scope_keys.add(key)
-        return new_scope_keys
+    def get_new_scope_keys(self, new_items, p_prime, p_idxs, limit):
+        for new_limit, limit_idxs in new_items:
+            if new_limit not in p_prime:
+                inter_len = p_idxs.intersection_len(limit_idxs)
+                if inter_len >= self.min_support:
+                    new_pidxs = p_idxs.intersection(limit_idxs)
+                    yield limit, new_pidxs
 
-    def _inner(self, p, p_idxs, limit, scope_keys):
-        keys = scope_keys - p
-        cp = {k for k in keys if p_idxs.issubset(self.item_to_tids[k])}
+    def _inner(self, p, p_idxs, limit, scope_items):
+        cp = (k for k, idxs in reversed(scope_items) if p_idxs.issubset(idxs))
 
-        max_k = max(cp, default=0)
-        if max_k and max_k <= limit:
+        max_k = next(cp, None)
+        if max_k and max_k == limit:
+            cp = set(cp).union({max_k})
             p_prime = p.union(cp)
             yield self.format(p_prime, p_idxs)
 
-            new_scope_keys = self.get_new_scope_keys(keys - p_prime, p_idxs)
+            new_items = self.item_to_tids.items()[:self.item_to_tids.bisect(limit)]
+            for new_limit, new_pidxs in self.get_new_scope_keys(new_items, p_prime, p_idxs, limit):
+                yield from self._inner(p_prime, new_pidxs, new_limit, scope_items)
 
-            for new_limit in new_scope_keys:
-                if new_limit < limit:
-                    new_pidxs = p_idxs.intersection(self.item_to_tids[new_limit])
-                    yield from self._inner(p_prime, new_pidxs, new_limit, new_scope_keys)
-
-            new_scope_keys.clear()
-
-        cp.clear()
+            cp.clear()
